0548a9c79df06de10f0063b37a1bfb2a47efb484
jordanparnold222 <p.jordanarnold@gmail.com>
2022-05-16 15:20:52 -0700

    End of day progress

--- ng-fundamentals/ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/ng-fundamentals/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { NavComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
